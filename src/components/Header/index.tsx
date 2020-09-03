5646c8822d3909e8211146e9f383ee46952e3823
httpchris <guilherme.costa@bandtec.com.br>
2020-09-03 20:47:37 -0300

    configurações padrões + estilização básica

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      Interface produzida com a Rocketseat
+    </Container>
+  );
+}
+
+export default Header;
